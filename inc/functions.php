680434470d8f9c4698eb988b474dbcd8c9dab2ac
Josh Wilson <63858777+joshwilsontas@users.noreply.github.com>
2023-10-16 17:15:10 +1100

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -7,20 +7,4 @@ function dd($value) {
     die();
 }
 
-function urlIs($value) {
-    return $_SERVER['REQUEST_URI'] == $value;
-}
 
-
-// Read the domain name of the current page
-$domain = $_SERVER['HTTP_HOST'];
-//Read the requested resource
-$resource = $_SERVER['REQUEST_URI'];
-// Find out the protocol of the current url
-if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-$protocol = "https";
-else
-$protocol = "http";
-
-// Combine all parts to get the full URL address
-$currentpageurl = $protocol."://".$domain.$resource;
