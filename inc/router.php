a00b405cbdce3615cd3968b00a1a3d1226f5c325
Josh Wilson <63858777+joshwilsontas@users.noreply.github.com>
2023-10-16 17:16:54 +1100

    Update router.php

--- inc/router.php
+++ inc/router.php
@@ -7,22 +7,9 @@ $uri = parse_url($dir.$_SERVER['REQUEST_URI'])['path'];
 
 $routes = [
     '/'             => 'controllers/index.php',
-    '/about'        => 'controllers/about.php',
-    '/artists'      => 'controllers/artists.php',
-    '/artist'       => 'controllers/artist.php',
-    '/profile'      => 'controllers/profile.php',
-    '/support'     => 'controllers/support.php',
-    '/login'        => 'controllers/login.php',
-    '/register'     => 'controllers/register.php',
-    '/release'      => 'controllers/release.php',
-    '/events'       => 'controllers/events.php',
-    '/venues'       => 'controllers/venues.php',
-    '/venue'        => 'controllers/venue.php',
-    '/news'         => 'controllers/news.php',
-    '/article' => 'controllers/article.php',
-    '/privacy'      => 'controllers/privacy.php',
-    '/forgot'       => 'controllers/forgot.php',
-    '/data-request' => 'controllers/data-request.php',
+    // This is how you route files 
+    // '/about'         => 'controllers/about.php',
+
 ];
 
 if(array_key_exists($uri, $routes)) {
