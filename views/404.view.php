6facada2e8ff7b14e9112db7e58add29ca6eea9b
Josh Wilson <63858777+joshwilsontas@users.noreply.github.com>
2023-10-23 13:33:25 +1100

    Create 404.view.php

---
+++ views/404.view.php
@@ -0,0 +1,16 @@
+<?php require('partials/head.php'); ?>
+	<!-- header -->
+	<?php require('partials/top-nav.php'); ?>
+	<!-- end header -->
+
+	<!-- sidebar -->
+	<?php require ('partials/sidebar.php'); ?>
+	<!-- end sidebar -->
+
+
+<!-- Page Content Goes Here -->
+
+Page Not Found. Please Try again
+
+	
+<?php require('partials/footer.php');?>
