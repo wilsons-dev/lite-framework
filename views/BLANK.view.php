422b21d1dce606fa79c2e5645b439d5694fb7487
Josh Wilson <63858777+joshwilsontas@users.noreply.github.com>
2023-11-01 11:26:21 +1100

    Update BLANK.view.php

--- views/BLANK.view.php
+++ views/BLANK.view.php
@@ -1,16 +1,1 @@
-<?php require('partials/head.php'); ?>
-	<!-- header -->
-	<?php require('partials/top-nav.php'); ?>
-	<!-- end header -->
 
-	<!-- sidebar -->
-	<?php require ('partials/sidebar.php'); ?>
-	<!-- end sidebar -->
-
-
-<!-- Page Content Goes Here -->
-
-
-
-	
-<?php require('partials/footer.php');?>
