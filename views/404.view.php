9d2e6bdfaba5201e06821b7374dea3a7db28bc63
Josh Wilson <63858777+joshwilsontas@users.noreply.github.com>
2023-11-01 11:32:16 +1100

    Update 404.view.php

--- views/404.view.php
+++ views/404.view.php
@@ -1,11 +1,3 @@
-<?php require('partials/head.php'); ?>
-	<!-- header -->
-	<?php require('partials/top-nav.php'); ?>
-	<!-- end header -->
-
-	<!-- sidebar -->
-	<?php require ('partials/sidebar.php'); ?>
-	<!-- end sidebar -->
 
 
 <!-- Page Content Goes Here -->
@@ -13,4 +5,3 @@
 Page Not Found. Please Try again
 
 	
-<?php require('partials/footer.php');?>
